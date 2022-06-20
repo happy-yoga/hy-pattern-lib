c4527527fecd6c3f825ca44bf24a0e4fb2fd788d
Falk Hoppe <falkhoppe81@gmail.com>
2022-06-20 10:57:33 +0200

    minimal pattern lib setup

---
+++ faucet.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  watchDirs: ['./bin', './lib'],
+  js: [{
+    source: './lib/js/index.js',
+    target: './dist/hy-components.js'
+    // esnext: true
+  }],
+
+  sass: [{
+    source: './lib/css/main.scss',
+    target: './dist/hy-components.css'
+  }],
+
+  manifest: {
+    target: './dist/manifest.json',
+    key: 'short',
+    baseURI: './'
+  }
+}
